7009e6a5a533ff31314db1cb65848ec8a084070e
Erik Henrique <erikhrodrigues@gmail.com>
2022-11-03 11:39:50 -0300

    Adicionando media queries

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 let btn = document.querySelector("#btn-share");
 let btnActive = document.querySelector("#btn-share-active");
 let share = document.querySelector(".article__content__share");
+let imgBtn = document.querySelector("#img-btn")
 
 btn.addEventListener("click", () => {
     share.classList.toggle("show");
+    if(share.classList.contains("show")){
+        imgBtn.setAttribute('src', "../img/share-active.svg")
+    } else {
+        imgBtn.setAttribute('src', "../img/share.svg")
+    }
 });
 
 btnActive.addEventListener("click", () => {
