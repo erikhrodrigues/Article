d37b367155806145a8294a29576d21092a7597fd
Erik Henrique <erikhrodrigues@gmail.com>
2022-11-04 06:10:12 -0300

    Otimizando Script para troca de imagem

--- js/main.js
+++ js/main.js
@@ -2,17 +2,22 @@
 let btn = document.querySelector("#btn-share");
 let btnActive = document.querySelector("#btn-share-active");
 let share = document.querySelector(".article__content__share");
-let imgBtn = document.querySelector("#img-btn")
+let imgBtn = document.querySelector("#img-btn");
 
 btn.addEventListener("click", () => {
     share.classList.toggle("show");
-    if(share.classList.contains("show")){
-        imgBtn.setAttribute('src', "img/share-active.svg")
-    } else {
-        imgBtn.setAttribute('src', "img/share.svg")
-    }
+    alterarBtnShare();
 });
 
 btnActive.addEventListener("click", () => {
     share.classList.toggle("show");
+    alterarBtnShare();
 });
+
+function alterarBtnShare() {
+    if(share.classList.contains("show")){
+        imgBtn.setAttribute('src', "img/share-active.svg");
+    } else {
+        imgBtn.setAttribute('src', "img/share.svg");
+    }
+}
